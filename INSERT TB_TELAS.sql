17c2df9aea9d6b9dd33418c4bad69d607d12155c
carlosmoura2018 <37423908+carlosmoura2018@users.noreply.github.com>
2018-08-27 22:14:33 -0300

    INSERT REGISTRO NA TABELA TB_TELAS
    SCRIPT SQL CONTENDO OS INSERTS COM OS NOMES DAS TELAS DO SISTEMA.

---
+++ INSERT TB_TELAS.sql
@@ -0,0 +1,14 @@
+USE BD_SYSREL;
+
+INSERT INTO `tb_telas` (`ID`,`DESCRICAO`) VALUES (1,'Cadastro de Usuários');
+INSERT INTO `tb_telas` (`ID`,`DESCRICAO`) VALUES (2,'Permissões de Usuário');
+INSERT INTO `tb_telas` (`ID`,`DESCRICAO`) VALUES (3,'Cadastro de Grupo de Pesquisa');
+INSERT INTO `tb_telas` (`ID`,`DESCRICAO`) VALUES (4,'Cadastro de Linhas de Pesquisa');
+INSERT INTO `tb_telas` (`ID`,`DESCRICAO`) VALUES (5,'Cadastro de Técnicos');
+INSERT INTO `tb_telas` (`ID`,`DESCRICAO`) VALUES (6,'Cadastro de Docentes');
+INSERT INTO `tb_telas` (`ID`,`DESCRICAO`) VALUES (7,'Cadastro de Discentes');
+INSERT INTO `tb_telas` (`ID`,`DESCRICAO`) VALUES (8,'Cadastro de Projetos de Pesquisa');
+INSERT INTO `tb_telas` (`ID`,`DESCRICAO`) VALUES (9,'Cadastro de Publicações');
+INSERT INTO `tb_telas` (`ID`,`DESCRICAO`) VALUES (10,'Cadastro de Equipamentos');
+INSERT INTO `tb_telas` (`ID`,`DESCRICAO`) VALUES (11,'Cadastro de Reuniões');
+INSERT INTO `tb_telas` (`ID`,`DESCRICAO`) VALUES (12,'Relatório');
