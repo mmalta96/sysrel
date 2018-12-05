5dd4756e12de665cd8363021a4fb55a0c224b852
Matheus Malta <matheusmalta.p@gmail.com>
2018-12-05 13:31:34 -0200

    Update VerificaRecuperacaoSenha.php

--- VerificaRecuperacaoSenha.php
+++ VerificaRecuperacaoSenha.php
@@ -190,12 +190,12 @@ try {
      $mail->Host = 'mx1.hostinger.com.br';
      $mail->SMTPAuth   = true;  
      $mail->Port       = 587; 
-     $mail->Username = 'contato@sysrel.tk'; 
+     $mail->Username = 'contato@sysrel.ml'; 
      $mail->Password = '183461'; 
  
      //Define o remetente
-     $mail->SetFrom('contato@sysrel.tk', 'Matheus - Sysrel'); 
-     $mail->AddReplyTo('contato@sysrel.tk', 'Matheus - Sysrel'); 
+     $mail->SetFrom('contato@sysrel.ml', 'Matheus - Sysrel'); 
+     $mail->AddReplyTo('contato@sysrel.ml', 'Matheus - Sysrel'); 
      $mail->Subject = 'Recuperacao de Senha';//Assunto do e-mail
  
  
